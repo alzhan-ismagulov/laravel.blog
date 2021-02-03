a7bd6a00580b1904e253eb07f9269078b3aaabb0
alzhan-ismagulov <aismagulov@gmail.com>
2021-02-03 13:34:42 +0100

    Commit 07. CRUD Categories

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -26,18 +26,23 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.categories.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title'=>'required',
+        ]);
+        Category::create($request->all());
+        $request->session()->flash('success', 'Категория добавлена');
+        return redirect()->route('categories.index');
     }
 
     /**
@@ -59,7 +64,8 @@ class CategoryController extends Controller
      */
     public function edit($id)
     {
-        dd(__METHOD__);
+        $category = Category::find($id);
+        return view('admin.categories.edit', compact('category'));
     }
 
     /**
@@ -67,11 +73,16 @@ class CategoryController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'title'=>'required',
+        ]);
+        $category = Category::find($id);
+        $category->update($request->all());
+        return redirect()->route('categories.index')->with('success', 'Изменения сохранены');
     }
 
     /**
@@ -82,6 +93,9 @@ class CategoryController extends Controller
      */
     public function destroy($id)
     {
-        dd(__METHOD__);
+//        $category = Category::find($id);
+//        $category->delete();
+        Category::destroy($id);
+        return redirect()->route('categories.index')->with('success', 'Категория удалена');
     }
 }
