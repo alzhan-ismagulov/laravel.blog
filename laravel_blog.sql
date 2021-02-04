c66e499974722cebbd58fdffc58297765aa584b5
alzhan-ismagulov <aismagulov@gmail.com>
2021-02-04 13:20:07 +0100

    Commit 12. Registration

--- laravel_blog.sql
+++ laravel_blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Фев 03 2021 г., 03:21
+-- Время создания: Фев 04 2021 г., 15:18
 -- Версия сервера: 5.7.29
 -- Версия PHP: 7.4.4
 
@@ -34,6 +34,16 @@ CREATE TABLE `categories` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `categories`
+--
+
+INSERT INTO `categories` (`id`, `title`, `slug`, `created_at`, `updated_at`) VALUES
+(1, 'Категория №1', 'kategoriya-1', '2021-02-03 11:04:45', '2021-02-03 11:04:45'),
+(2, 'Категория №2', 'kategoriya-2', '2021-02-03 11:06:28', '2021-02-03 11:06:28'),
+(3, 'Категория №3', 'kategoriya-3', '2021-02-03 11:06:54', '2021-02-03 11:06:54'),
+(5, 'Категория №4', 'kategoriya-4', '2021-02-03 11:30:49', '2021-02-03 11:30:49');
 
 -- --------------------------------------------------------
 
@@ -67,12 +77,14 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2019_08_19_000000_create_failed_jobs_table', 1),
-(3, '2021_02_03_000646_create_categories_table', 1),
-(4, '2021_02_03_000717_create_tags_table', 1),
-(5, '2021_02_03_000738_create_posts_table', 1),
-(6, '2021_02_03_000823_create_post_tag_table', 1);
+(7, '2014_10_12_000000_create_users_table', 1),
+(8, '2019_08_19_000000_create_failed_jobs_table', 1),
+(9, '2021_02_03_000646_create_categories_table', 1),
+(10, '2021_02_03_000717_create_tags_table', 1),
+(11, '2021_02_03_000738_create_posts_table', 1),
+(12, '2021_02_03_000823_create_post_tag_table', 1),
+(13, '2021_02_03_152124_create_tag_controllers_table', 2),
+(14, '2021_02_04_114753_alter_table_users_add_isadmin', 2);
 
 -- --------------------------------------------------------
 
@@ -93,6 +105,14 @@ CREATE TABLE `posts` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Дамп данных таблицы `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `slug`, `description`, `content`, `category_id`, `views`, `thumbnail`, `created_at`, `updated_at`) VALUES
+(3, 'Статья 1', 'statya-1', 'Цитата 1', 'Контент 1', 1, 0, '', '2021-02-03 22:17:08', '2021-02-03 22:17:08'),
+(4, 'Статья 2', 'statya-2', 'Цитата 2', 'Контент 2', 2, 0, NULL, '2021-02-03 22:23:23', '2021-02-03 23:25:32');
+
 -- --------------------------------------------------------
 
 --
@@ -107,6 +127,14 @@ CREATE TABLE `post_tag` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Дамп данных таблицы `post_tag`
+--
+
+INSERT INTO `post_tag` (`id`, `tag_id`, `post_id`, `created_at`, `updated_at`) VALUES
+(1, 1, 4, NULL, NULL),
+(2, 2, 4, NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -117,6 +145,27 @@ CREATE TABLE `tags` (
   `id` int(10) UNSIGNED NOT NULL,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `tags`
+--
+
+INSERT INTO `tags` (`id`, `title`, `slug`, `created_at`, `updated_at`) VALUES
+(1, 'Тег №1', 'teg-1', '2021-02-03 14:26:05', '2021-02-03 23:04:17'),
+(2, 'Тег №2', 'teg-2', '2021-02-03 14:31:22', '2021-02-03 23:04:27'),
+(4, 'Тэг №3', 'teg-3', '2021-02-03 15:25:55', '2021-02-03 15:25:55');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `tag_controllers`
+--
+
+CREATE TABLE `tag_controllers` (
+  `id` bigint(20) UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -135,8 +184,17 @@ CREATE TABLE `users` (
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `is_admin` tinyint(4) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `is_admin`) VALUES
+(1, 'Admin', 'admin@mail.com', NULL, '$2y$10$JWGnUoW2LwZQKmvv4Mdw1Ow1SLgiNb99uO5Gy5OioXzDYl9nZphTC', NULL, '2021-02-04 11:15:49', '2021-02-04 11:15:49', 1),
+(2, 'User', 'user@mail.com', NULL, '$2y$10$A3e799Tba/Xwh4ObyDeWkefV9ssIWtBC91k4OdgvAcobkLt3u2Xye', NULL, '2021-02-04 11:17:33', '2021-02-04 11:17:33', 0);
 
 --
 -- Индексы сохранённых таблиц
@@ -146,7 +204,8 @@ CREATE TABLE `users` (
 -- Индексы таблицы `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `categories_slug_unique` (`slug`);
 
 --
 -- Индексы таблицы `failed_jobs`
@@ -164,7 +223,8 @@ ALTER TABLE `migrations`
 -- Индексы таблицы `posts`
 --
 ALTER TABLE `posts`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `posts_slug_unique` (`slug`);
 
 --
 -- Индексы таблицы `post_tag`
@@ -176,6 +236,13 @@ ALTER TABLE `post_tag`
 -- Индексы таблицы `tags`
 --
 ALTER TABLE `tags`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `tags_slug_unique` (`slug`);
+
+--
+-- Индексы таблицы `tag_controllers`
+--
+ALTER TABLE `tag_controllers`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -193,7 +260,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT для таблицы `failed_jobs`
@@ -205,31 +272,37 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT для таблицы `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT для таблицы `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `post_tag`
 --
 ALTER TABLE `post_tag`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT для таблицы `tags`
 --
 ALTER TABLE `tags`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `tag_controllers`
+--
+ALTER TABLE `tag_controllers`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
