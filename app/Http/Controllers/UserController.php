f64d1dbc6eb2eb6d765fd31e81ebae45726e70d4
alzhan-ismagulov <aismagulov@gmail.com>
2021-02-12 21:35:51 +0100

    Commit 13. Authorization

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
+
     public function create()
     {
         return view('user.create');
@@ -24,11 +25,43 @@ class UserController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => bcrypt($request->passord),
+            'password' => bcrypt($request->password),
         ]);
 
-        session()->flash('succecc', 'Регистрация пройдена');
+        session()->flash('success', 'Регистрация пройдена');
         Auth::login($user);
         return redirect()->home();
     }
+
+    public function loginForm()
+    {
+        return view('user.login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        if (Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password,
+        ])){
+            session()->flash('success', 'You are logged');
+            if (Auth::user()->is_admin){
+                return redirect()->route('admin.index');
+            } else {
+                return redirect()->home();
+            }
+        }
+        return redirect()->back()->with('error', 'Incorrect login or password');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('login.create');
+    }
+
 }
